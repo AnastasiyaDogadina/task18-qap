478cfca3b118525f7560c5a47b6da2eba1e5a297
ShikunovArtem <34123955+Popokatepetl007@users.noreply.github.com>
2023-02-27 02:48:27 +0700

    немного исправлений

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ print("********************************************")
 print("************* ПРОДАЖА БИЛЕТОВ! *************")
 print("********************************************")
 
-is_right = False # Переменная добавленная для проверки корректности введенных данных, пока False не дает программе идти дальше
-count_of_tickets = 0 # Кол-во билетов
+# Переменная добавленная для проверки корректности введенных данных, пока False не дает программе идти дальше
+is_right = False
+count_of_tickets = 0  # Кол-во билетов
 while not is_right:
     try:
         count_of_tickets = int(input("Введите необходимое кол-во билетов: "))
@@ -15,29 +16,32 @@ while not is_right:
         is_right = True
     except ValueError:
         print("Введите корректное значение")
-cost = 0
+
+cost = 0  # Общая стоимость
 
 for person in range(count_of_tickets):
     is_right = False
-    age = 0 # Возраст человека
+    age = 0  # Возраст человека
     while not is_right:
         try:
-            age = int(input('Введите возраст посетителя номер {} '.format(person + 1)))
+            age = int(input(f'Введите возраст посетителя номер {person + 1}: '))
             if age < 0:
                 raise ValueError()
+            if age > 100:
+                print('\nМы вам поверим, но мы вам не верим -_- \n')
             is_right = True
         except ValueError:
-            print("Введите корректное значение")
+            print("Введите корректное значение (положительное число)")
 
     if age >= 25:
-        cost = cost + 1390
+        cost += 1390
     elif 18 <= age < 25:
-        cost = cost + 990
+        cost += 990
     else:
         continue
 
 if count_of_tickets > 2:
-    cost = cost * 0.9
-
-print("Общая стоимость билетов:", cost, "рублей")
+    cost *= 0.9
+print("\nРезультат:")
+print(f"Общая стоимость билетов{' (с учетом скидки)' if count_of_tickets > 2 else ''}:", cost, "рублей")
 print("Хорошего дня!")
